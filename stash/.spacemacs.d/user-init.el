5bb16ebb46f25497c388abaa6954fc6885dfe779
Eric Walkingshaw <eric@walkingshaw.net>
2018-02-19 22:59:18 -0800

    Add currently unused emacs configs.

---
+++ stash/.spacemacs.d/user-init.el
@@ -0,0 +1,13 @@
+;; Loaded by "init.el".
+
+;; Place for any initialization of user code to be executed before
+;; layers/packages are loaded.
+(defun dotspacemacs/user-init ()
+
+  "Initialization function for user code."
+
+  ;; Make comments less ugly
+  (setq spacemacs-theme-comment-bg nil)
+  (setq spacemacs-theme-comment-italic f)
+
+  )
