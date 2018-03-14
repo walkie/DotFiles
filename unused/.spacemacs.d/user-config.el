bb988477a0487dd51fc34fa26ffb91a2ac8aa323
Eric Walkingshaw <eric@walkingshaw.net>
2018-03-13 23:45:42 -0700

    Delete unused configs.

--- unused/.spacemacs.d/user-config.el
+++
@@ -1,9 +0,0 @@
-;; Loaded by "init.el".
-
-;; Called after layers/packages are loaded. This is the place where most
-;; user configuration should live.
-(defun dotspacemacs/user-config ()
-
-  "Configuration function for user code."
-
-  )
